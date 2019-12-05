cccf466281edfe01317fda8fd8b4fbb6ba05e52e
DavidP-1993 <davidpegoraro93@gmail.com>
2019-12-05 13:27:19 -0300

    Carousel

--- carousel.php
+++ carousel.php
@@ -8,15 +8,15 @@
             <li data-target="#myCarousel" data-slide-to="2"></li>
         </ol>
         <!-- Wrapper for carousel items -->
-        <div class="carousel-inner">
+        <div class="carousel-inner" style="background-color: rgba(0, 0, 0, 0.5);" role="listbox">
             <div class="carousel-item active">
-                <img src="img/av1.jpeg" alt="First Slide">
+                <img src="img/av1.jpeg" alt="First Slide" width="600" height="700">
             </div>
             <div class="carousel-item">
-                <img src="img/av2.jpeg" alt="Second Slide">
+                <img src="img/av2.jpeg" alt="Second Slide" width="600" height="700">
             </div>
             <div class="carousel-item">
-                <img src="img/av3.jpeg" alt="Third Slide">
+                <img src="img/av3.jpeg" alt="Third Slide" width="600" height="700">
             </div>
         </div>
         <!-- Carousel controls -->
