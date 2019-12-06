a18022df134dfc70f00bdb763afaacd4f68e5551
unknown <alanbersia@gmail.com>
2019-12-06 02:43:50 -0300

    actualizacion pagina equipo

--- equipo.php
+++ equipo.php
@@ -8,16 +8,78 @@
     <!--BOOTSTRAP CSS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <!--CSS-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!--FONTAWESOME-->
+    <script src="https://kit.fontawesome.com/35ffb07adf.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <!--CSS-->
     <link rel="stylesheet" href="css/estilos.css">
-     <!--FONTS-->
-     <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
-     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <!--FONTS-->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 </head>
 <body>
 <?php require_once "header.php";
 require_once "navbar.php";
 ?>
+<br><br>
+<div class="container">
+<h1 class="display-4">El Plantel- <strong>PRIMERA DIVISIÓN</strong></h1>
+<img src="img/av3.jpeg" alt="">
+<div class="row">
+    <div class="col-lg-3">
+
+    <div class="card">
+                        <div class="card-body jugador">
+                            <a href="/" style="text-decoration:none; color:black">
+                            <img src="img/person.png" class="img-fluid rounded-circle" style="width: 50%;display:block;margin:auto;" alt="person">
+                            <p>Mediocampista</p>
+                            <h3>ALAN BERSIA</h3>
+                            <h2 class="numeros display-4">10</h2>
+
+                            <div class="d-flex flex-row justify-content-center">
+                                <div class="p-4">
+                                    <a href="https://facebook.com/alan.bersia" target="blank">
+                                        <i class="fab fa-facebook-square"></i>
+                                    </a>
+                                </div>
+                                <div class="d-flex flex-row justify-content-center">
+                                    <div class="p-4">
+                                        <a href="https://twitter.com/home" target="blank">
+                                            <i class="fab fa-twitter"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-row justify-content-center">
+                                    <div class="p-4">
+                                        <a href="#" target="blank">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+    <div class="col-md-3">
+
+    </div>
+    <div class="col-md-3">
+
+    </div>
+    <div class="col-md-3">
+        
+    </div>
+</div>
+</div>
 
 
 
@@ -25,19 +87,7 @@ require_once "navbar.php";
 
 
 
-
-
-    <!--BOOTSTRAP JAVASCRIPT-->
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+  
     <!--SCRIPT NAVBAR-->
     <script>
       window.onscroll = function () { myFunction() };
