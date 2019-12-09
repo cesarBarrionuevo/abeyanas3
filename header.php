0a999cf405fd491f31907a8d925cbb7e12209bb2
DavidP-1993 <davidpegoraro93@gmail.com>
2019-12-09 06:24:48 -0300

    Cambios header

--- header.php
+++ header.php
@@ -1,16 +1,32 @@
-<div class="d-none d-sm-block">
+<div class="d-none d-md-block">
   <header>
+    <!-- Primer encabezado-->
       <div class="row partidos m-auto">
-            <p class="leyenda"><strong style="color:red;">próximo partido </strong>| liga venadense <strong>| 30/11/2019 | 17:00 hs | </strong> visitante </p>
-               &nbsp;&nbsp;&nbsp;&nbsp;
-            <p style="padding-top:4px;"><img src="img/IMG_EscuEquipos/CSDChapuy.png" style="width: 16%;" alt=""></p>
+        <div class="leyenda1"><strong style="color:red;">próximo partido</strong></div>
+          <div class="vertical-line" ></div>
+        <div class="leyenda2">liga venadense</div>
+          <div class="vertical-line" ></div>
+        <div class="leyenda2"><strong>30/11/2019</strong></div>
+          <div class="vertical-line" ></div>
+        <p class="leyenda2"><strong>17:00 hs</strong></p>
+          <div class="vertical-line" ></div>
+        <p class="leyenda2">visitante</p>
+        <p class="img-header1"><img src="img/IMG_EscuEquipos/CSDChapuy.png" style="width: 16%;" alt=""></p>
+        <p class="img-header1"><img src="img/IMG_EscuEquipos/talleres.png" style="width: 16%;" alt=""></p>
+          <div class=" row m-auto">
+            <a class="img-sponsor-header"href="http://www.famago.com.ar"><img src="img/sponsor.png"
+            style="width: 70%;" alt=""></a>
+          </div>
+    <!--      -->
       </div>
-            <div class="container-fluid">
-              <div class="row encabezaLogo">
-                <a href="#">
-                  <p> <img src="img/header2.jpg" style="width: 100%;" alt="imagen"></p>
-                </a>
-              </div>
-            </div>
+
+      <!-- Segundo encabezado-->
+      <div class="container-fluid">
+        <div class="row encabezaLogo">
+          <a href="#">
+            <p> <img src="img/header2.jpg" style="width: 100%;" alt="imagen"></p>
+          </a>
+        </div>
+      </div>
   </header>
 </div>
