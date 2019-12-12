4f2ef31bfba300ab17e278045d6cc44c2d5cf8f8
Cesar Barrionuevo <saavedra0706@gmail.com>
2019-12-12 12:52:11 -0300

    cambios footer

--- footer.php
+++ footer.php
@@ -1,8 +1,35 @@
      <footer class="py-1">
-
-               <div class="imagen-footer">
+          <div class="row">
+               <div class="imagen-footer col-lg-6 col-md-6 col-sm-6">
                     <a href="#">
-                    <img  src="img/escudo.png"  width="120" height="120" alt="escudp.png"></a>
+                    <img class="img-fluid" src="img/escudoFooter.png" alt="escudoFooter.png"></a>
+                    <h3 class="nombre-club"><strong class="texto-borde">ClubSportivo</strong><strong style="color: red">Avellaneda</strong></h3>
+               </div>
+               <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="text-right col-lg-7 col-md-6 col-sm-2">
+                         <h6 class="text-primary">Seguinos en:</h6>
+                    </div>
+                    <div class="redes-ul col-lg-5 col-md-6 col-sm-10">
+                         <ul class="">
+                             <li style="list-style:none;">
+                                  <a href="https://www.facebook.com/sportivo.avellaneda" target="blank" class="etiqueta-red" title="Ir a nuestra página de facebook">
+
+                                      <i id="redes-footer" class="fab fa-facebook-square fa-3x"></i>
+                                    </a>
+                             </li>
+                             <li style="list-style:none;">
+                                  <a href="https://www.facebook.com/sportivo.avellaneda" target="blank" class="etiqueta-red" title="Ir a nuestra página de Instagram">
+                                      <i id="redes-footer" class="fab fa-instagram fa-3x"></i>
+                                 </a>
+                             </li>
+                         </ul>
+                    </div>
+
+
+               </div>
+     <!--          <div class="imagen-footer">
+                    <a href="#">
+                    <img class="img-fluid" src="img/escudoFooter.png" alt="escudoFooter.png"></a>
                     <h3 class="nombre-club"><strong class="texto-borde">ClubSportivo</strong><strong style="color: red">Avellaneda</strong></h3>
                </div>
 
@@ -22,8 +49,8 @@
                        </li>
                    </ul>
                </div>
-
-
+               -->
+          </div>
 
                <div class="copyright py-3">
                    <p>Club Sportivo Avellaneda © 2019 Todos los derechos reservados</p>
