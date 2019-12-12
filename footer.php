35bb035d099ebb5d1685966903406303f06cb8c1
Cesar Barrionuevo <saavedra0706@gmail.com>
2019-12-12 11:16:43 -0300

    cambios al footer

--- footer.php
+++ footer.php
@@ -1,33 +1,40 @@
      <footer class="py-1">
-       
-         <div class="imagen-footer">
-         <a href="#">
-         <img  src="img/escudo.png"  width="150" height="150" alt="escudp.png"></a> 
-         <h3 class="nombre-club"><strong class="texto-borde">ClubSportivo</strong><strong style="color: red">Avellaneda</strong></h3>
-         </div>
 
-         <div class="redes">
-             <span>Seguinos en: </span>
-             <ul class="redes-ul">
-            <li style="list-style:none;">
-            <a href="https://www.facebook.com/sportivo.avellaneda" target="blank" class="etiqueta-red" title="Ir a nuestra página de facebook">
-              
-              <i id="redes-footer" class="fab fa-facebook-square fa-3x"></i>
-              </a>
-            </li>
-            <li style="list-style:none;">
-            <a href="https://www.facebook.com/sportivo.avellaneda" target="blank" class="etiqueta-red" title="Ir a nuestra página de Instagram">
-            <i id="redes-footer" class="fab fa-instagram fa-3x"></i>
-        </a>
-            </li>
-             </ul>
-         </div>
-         
-         <div class="copyright py-5">
-             <p>Club Sportivo Avellaneda © 2019 Todos los derechos reservados, Sede San Martín & Avellaneda, Venado Tuerto, Santa Fe, Argentina · Teléfono  03462 - 15305030 ·</p>
-             </div>
-             <div class="desarrolladores">
+               <div class="imagen-footer">
+                    <a href="#">
+                    <img  src="img/escudo.png"  width="120" height="120" alt="escudp.png"></a>
+                    <h3 class="nombre-club"><strong class="texto-borde">ClubSportivo</strong><strong style="color: red">Avellaneda</strong></h3>
+               </div>
+
+               <div class="redes">
+                   <span>Seguinos en: </span>
+                   <ul class="redes-ul">
+                       <li style="list-style:none;">
+                            <a href="https://www.facebook.com/sportivo.avellaneda" target="blank" class="etiqueta-red" title="Ir a nuestra página de facebook">
+
+                                <i id="redes-footer" class="fab fa-facebook-square fa-3x"></i>
+                              </a>
+                       </li>
+                       <li style="list-style:none;">
+                            <a href="https://www.facebook.com/sportivo.avellaneda" target="blank" class="etiqueta-red" title="Ir a nuestra página de Instagram">
+                                <i id="redes-footer" class="fab fa-instagram fa-3x"></i>
+                           </a>
+                       </li>
+                   </ul>
+               </div>
+
+
+
+               <div class="copyright py-3">
+                   <p>Club Sportivo Avellaneda © 2019 Todos los derechos reservados</p>
+                   <p>Sede San Martín & Avellaneda - Venado Tuerto - Santa Fe - Argentina</p>
+                   <p>mail: administración@sportivoavellaneda.com.ar -  Teléfono  03462 - 15305030 </p>
+               </div>
+
+
+
+              <div class="desarrolladores pb-2">
                  <strong><p>Sitio desarrollado por Abeyabas3 ®</p></strong>
              </div>
-            
-     </footer>
+
+   </footer>
