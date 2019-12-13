0b43588e7c36ece9d497ada52abcbe3294211ed5
DavidP-1993 <davidpegoraro93@gmail.com>
2019-12-13 02:50:05 -0300

    Nuevo carousel

---
+++ carousel2.php
@@ -0,0 +1,62 @@
+<div class="contain">
+  <div class="row">
+    <div class="row__inner">
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="img/av1.jpeg" alt="">
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Top Gear
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="img/av2.jpeg" alt="">
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Top Gear
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="img/av3.jpeg" alt="">
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Top Gear
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="img/av4.jpeg" alt="">
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Top Gear
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <div class="tile__media">
+          <img class="tile__img" src="img/av5.jpeg" alt="">
+        </div>
+        <div class="tile__details">
+          <div class="tile__title">
+            Top Gear
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -2,79 +2,58 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SportivoAvellaneda</title>
-    <!--BOOTSTRAP CSS-->
-    <link rel="icon" type="image/png" href="img/avellaneda.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="img/avellaneda.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="img/avellaneda.png" sizes="64x64">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SportivoAvellaneda</title>
+  <!--BOOTSTRAP CSS-->
+  <link rel="icon" type="image/png" href="img/avellaneda.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="img/avellaneda.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="img/avellaneda.png" sizes="64x64">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <!--FONTAWESOME-->
-    <script src="https://kit.fontawesome.com/35ffb07adf.js" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
-    <!--CSS-->
-    <link rel="stylesheet" href="css/estilos.css">
-    <!--FONTS-->
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+  <!--FONTAWESOME-->
+  <script src="https://kit.fontawesome.com/35ffb07adf.js" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+  crossorigin="anonymous"></script>
+  <!--CSS-->
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="css/lightbox.css">
+  <!--FONTS-->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body style="height: 1500px">
 
 
-    <!--HEADER-->
+  <!--HEADER-->
 
-   <?php require_once "header.php" ?>
+  <?php require_once "header.php" ?>
 
-    <!--NAVEGACIÓN-->
+  <!--NAVEGACIÓN-->
 
-   <?php require_once "navbar.php" ?>
-   <?php require_once "carousel.php" ?>
+  <?php require_once "navbar.php" ?>
+  <?php require_once "carousel2.php" ?>
 
-    <div class="container">
-      <div class="container-noticias">
-        asdasdasgfaggA
-      </div>
+  <div class="container">
+    <div class="container-noticias">
+      asdasdasgfaggA
     </div>
+  </div>
 
-    <!--FOOTER-->
-    <?php require_once "footer.php" ?>
+  <!--FOOTER-->
+  <?php require_once "footer.php" ?>
 
-    <!--SCRIPT NAVBAR-->
-    <script>
-      window.onscroll = function () { myFunction() };
-
-function myFunction() {
-    if (document.body.scrollTop > 245 || document.documentElement.scrollTop > 245) {
-        document.getElementById("escudo").className = "";
-        var x = document.getElementsByClassName("nav-item");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].classList.add('animacion');
-        }
-
-    } else {
-        document.getElementById("escudo").className = "d-none";
-        var x = document.getElementsByClassName("nav-item");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].classList.remove('animacion');
-        }
-    }
-}
-    </script>
-
+  <!--SCRIPT NAVBAR-->
+  <script src="js/funciones.js"></script>
 
 </body>
 </html>
