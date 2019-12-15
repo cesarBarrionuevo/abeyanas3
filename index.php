420d15249f7a6e4d30e2a5642bacdef2cf7bab9f
DavidP-1993 <davidpegoraro93@gmail.com>
2019-12-15 13:33:02 -0300

    Muchas cosas

--- index.php
+++ index.php
@@ -50,30 +50,9 @@
     </div>
     <!--FOOTER-->
     <?php require_once "footer.php" ?>
-    
+
     <!--SCRIPT NAVBAR-->
-    <script>
-      window.onscroll = function () { myFunction() };
-
-function myFunction() {
-    if (document.body.scrollTop > 142 || document.documentElement.scrollTop > 142) {
-        document.getElementById("escudo").className = "";
-        var x = document.getElementsByClassName("nav-item");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].classList.add('animacion');
-        }
-
-    } else {
-        document.getElementById("escudo").className = "d-none";
-        var x = document.getElementsByClassName("nav-item");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].classList.remove('animacion');
-        }
-    }
-}
-    </script>
+    <script src="js/funciones.js"></script>
 
 
 </body>
