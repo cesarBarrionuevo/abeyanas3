9695d0bc87b7f4a1bc8874268092426a0b15f5ad
DavidP-1993 <davidpegoraro93@gmail.com>
2019-12-16 18:53:16 -0300

    Update carousel

--- carousel2.php
+++ carousel2.php
@@ -1,13 +1,13 @@
 <div class="container-fluid">
   <div class="carousel" data-gap="20" data-bfc>
     <figure>
-      <img src="img/av1.jpeg" alt="">
-      <img src="img/av2.jpeg" alt="">
-      <img src="img/av3.jpeg" alt="">
-      <img src="img/av4.jpeg" alt="">
-      <img src="img/av5.jpeg" alt="">
       <img src="img/av6.jpg" alt="">
       <img src="img/av7.jpg" alt="">
+      <img src="img/av8.jpg" alt="">
+      <img src="img/av9.jpg" alt="">
+      <img src="img/av10.jpg" alt="">
+      <img src="img/av11.jpg" alt="">
+      <img src="img/av12.jpg" alt="">
       <img src="img/av8.jpg" alt="">
     </figure>
       <nav>
