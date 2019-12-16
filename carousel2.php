0f6a5f2227b12596703ca0534e1f02125014f322
DavidP-1993 <davidpegoraro93@gmail.com>
2019-12-16 18:34:43 -0300

    Nuevo carousel

--- carousel2.php
+++ carousel2.php
@@ -1,62 +1,18 @@
-<div class="contain">
-  <div class="row">
-    <div class="row__inner">
-
-      <div class="tile">
-        <div class="tile__media">
-          <img class="tile__img" src="img/av1.jpeg" alt="">
-        </div>
-        <div class="tile__details">
-          <div class="tile__title">
-            Top Gear
-          </div>
-        </div>
-      </div>
-
-      <div class="tile">
-        <div class="tile__media">
-          <img class="tile__img" src="img/av2.jpeg" alt="">
-        </div>
-        <div class="tile__details">
-          <div class="tile__title">
-            Top Gear
-          </div>
-        </div>
-      </div>
-
-      <div class="tile">
-        <div class="tile__media">
-          <img class="tile__img" src="img/av3.jpeg" alt="">
-        </div>
-        <div class="tile__details">
-          <div class="tile__title">
-            Top Gear
-          </div>
-        </div>
-      </div>
-
-      <div class="tile">
-        <div class="tile__media">
-          <img class="tile__img" src="img/av4.jpeg" alt="">
-        </div>
-        <div class="tile__details">
-          <div class="tile__title">
-            Top Gear
-          </div>
-        </div>
-      </div>
-
-      <div class="tile">
-        <div class="tile__media">
-          <img class="tile__img" src="img/av5.jpeg" alt="">
-        </div>
-        <div class="tile__details">
-          <div class="tile__title">
-            Top Gear
-          </div>
-        </div>
-      </div>
-
-    </div>
+<div class="container-fluid">
+  <div class="carousel" data-gap="20" data-bfc>
+    <figure>
+      <img src="img/av1.jpeg" alt="">
+      <img src="img/av2.jpeg" alt="">
+      <img src="img/av3.jpeg" alt="">
+      <img src="img/av4.jpeg" alt="">
+      <img src="img/av5.jpeg" alt="">
+      <img src="img/av6.jpg" alt="">
+      <img src="img/av7.jpg" alt="">
+      <img src="img/av8.jpg" alt="">
+    </figure>
+      <nav>
+      <button class="prev button"> < </button>
+      <button class="next button"> > </button>
+      </nav>
   </div>
 </div>
