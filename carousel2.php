2882e6370a9136eee451f205222093701be68529
Cesar Barrionuevo <saavedra0706@gmail.com>
2020-02-29 11:12:22 -0300

    actualizacion sitio pc cesar

--- carousel2.php
+++ carousel2.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <div class="container-fluid">
   <div class="carousel" data-gap="20" data-bfc>
     <figure>
@@ -16,3 +17,23 @@
       </nav>
   </div>
 </div>
+=======
+<div class="container-fluid">
+  <div class="carousel" data-gap="20" data-bfc>
+    <figure>
+      <img src="img/av6.jpg" alt="">
+      <img src="img/av7.jpg" alt="">
+      <img src="img/av8.jpg" alt="">
+      <img src="img/av9.jpg" alt="">
+      <img src="img/av10.jpg" alt="">
+      <img src="img/av11.jpg" alt="">
+      <img src="img/av12.jpg" alt="">
+      <img src="img/av8.jpg" alt="">
+    </figure>
+      <nav>
+      <button class="prev button"> < </button>
+      <button class="next button"> > </button>
+      </nav>
+  </div>
+</div>
+>>>>>>> 423f165feedb0cdce6dbc74278605eddac3a996d
---
+++ navigation1.php
@@ -0,0 +1,39 @@
+
+	<div class="cd-main-header">
+		<div class="row partidos m-auto d-block d-sm-block d-md-none mobile-nav">
+        <p class="leyenda1"><strong style="color:red;">próximo partido</strong></p><br>
+          <div class="vertical-line" ></div>
+        <p class="leyenda2">liga venadense</p>
+          <div class="vertical-line" ></div>
+        <p class="leyenda2"><strong>30/11/2019</strong></p>
+          <div class="vertical-line" ></div>
+        <p class="leyenda2"><strong>17:00 hs</strong></p>
+          <div class="vertical-line" ></div>
+        <p class="leyenda2">visitante</p>
+        <p id="img-eq1" class="img-header1"><img src="img/IMG_EscuEquipos/CSDChapuy.png" alt=""></p>
+        <p id="img-eq2" class="img-header1"><img src="img/IMG_EscuEquipos/talleres.png"  alt=""></p>
+    	<!--      <a class="img-sponsor-header"href="http://www.famago.com.ar"><img src="img/sponsor.png"
+    	style="width: 70%;" alt=""></a>-->
+      	</div>
+
+	
+		<ul>
+		<li class="d-none" id="escudo">
+                  <!---->
+                  <a href="#header" id="escudo-navbar" class=""><img src="img/escudo.png" width="50" height="52"
+                          ></a><p class="texto-borde-navbar"><strong>ClubSportivo</strong>Avellaneda</p>
+				</li>
+				
+		</ul>
+
+		<ul class="cd-header-buttons">
+      
+		
+			<li><a class="cd-nav-trigger" href="#cd-primary-nav"><span></span></a></li>
+         
+        </ul> <!-- cd-header-buttons -->
+       
+	</div>
+
+	<main class="cd-main-content" style="background:white url('img/dd1.png')">
+		
