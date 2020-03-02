e2c544bc8edf14a191d05555e25d2f63ffebdb29
Cesar Barrionuevo <saavedra0706@gmail.com>
2020-03-02 18:03:03 -0300

    arreglo de carusel

--- carousel2.php
+++ carousel2.php
@@ -1,5 +1,26 @@
 <div class="container-fluid">
-  <div class="carousel" data-gap="20" data-bfc>
+     <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+     <div class="carousel-inner">
+       <div class="carousel-item active" data-interval="10000">
+         <img src="img/av6.jpg" class="d-block w-100" alt="...">
+       </div>
+       <div class="carousel-item" data-interval="2000">
+         <img src="img/av7.jpg" class="d-block w-100" alt="...">
+       </div>
+       <div class="carousel-item">
+         <img src="img/av8.jpg" class="d-block w-100" alt="...">
+       </div>
+     </div>
+     <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+       <span class="sr-only">Previous</span>
+     </a>
+     <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+       <span class="carousel-control-next-icon" aria-hidden="true"></span>
+       <span class="sr-only">Next</span>
+     </a>
+   </div>
+  <!--<div class="carousel" data-gap="20" data-bfc>
     <figure>
       <img src="img/av6.jpg" alt="">
       <img src="img/av7.jpg" alt="">
@@ -14,5 +35,5 @@
       <button class="prev button"> < </button>
       <button class="next button"> > </button>
       </nav>
-  </div>
+</div> -->
 </div>
